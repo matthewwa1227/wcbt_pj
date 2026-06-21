24377c833e45a906f633933e66718a2312d6a15a
wcbt <antonwcbt@gmail.com>
2026-06-21 18:05:58 -0500

    Db Ports interfaces

--- backend/src/main/java/com/casualapp/backend/persistence/PersistenceFactory.java
+++ backend/src/main/java/com/casualapp/backend/persistence/PersistenceFactory.java
@@ -1,19 +1,28 @@
 package com.casualapp.backend.persistence;
 
-//import com.casualapp.backend.persistence.ports.EventLogPersistence;
-//import com.casualapp.backend.persistence.ports.JobAttendancePersistence;
+import com.casualapp.backend.persistence.ports.EventLogPersistence;
+import com.casualapp.backend.persistence.ports.JobAttendancePersistence;
 import com.casualapp.backend.persistence.ports.JobPersistence;
+import com.casualapp.backend.persistence.ports.JobRolePersistence;
 import com.casualapp.backend.persistence.ports.JobSignupPersistence;
 import com.casualapp.backend.persistence.ports.UserPersistence;
-//import com.casualapp.backend.persistence.ports.VenuePersistence;
-//import com.casualapp.backend.persistence.ports.WorkerProfilePersistence;
+import com.casualapp.backend.persistence.ports.VenuePersistence;
+import com.casualapp.backend.persistence.ports.WorkerProfilePersistence;
 
 public interface PersistenceFactory {
     UserPersistence users();
-    // WorkerProfilePersistence workerProfiles();
-    // VenuePersistence venues();
+
+    WorkerProfilePersistence workerProfiles();
+
+    JobRolePersistence jobRoles();
+
+    VenuePersistence venues();
+
     JobPersistence jobs();
+
     JobSignupPersistence jobSignups();
-    // JobAttendancePersistence jobAttendance();
-    // EventLogPersistence eventLogs();
+
+    JobAttendancePersistence jobAttendance();
+
+    EventLogPersistence eventLogs();
 }
